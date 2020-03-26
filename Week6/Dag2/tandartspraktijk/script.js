09ac9425eafc61a63a9968c1917313f12b65612f
kracok <kracok@gmail.com>
2020-03-26 14:28:49 +0100

    week 6

---
+++ Week6/Dag2/tandartspraktijk/script.js
@@ -0,0 +1,547 @@
+const dentists = [ 
+    {​
+    "id": 1,
+    "name": 'Hanneke van',
+    "surname": 'Veen',
+    "phone": '435324251',
+    "email": 'hanneke@tandartspraktijkbvt.nl',
+    "isSick": false
+    },
+    {
+    "id": 2,
+    "name": 'Mirthe',
+    "surname": 'Timmermans',
+    "phone": '425143532',
+    "email": 'Mirthe@tandartspraktijkbvt.nl',
+    "isSick": false
+    },
+    { 
+    "id": 3,
+    "name": 'Anne',
+    "surname": 'van der Laan',
+    "phone": '345234523',
+    "email": 'anne@tandartspraktijkbvt.nl',
+    "isSick": false
+    },
+    {
+    "id": 4,
+    "name": 'Anne',
+    "surname": 'van der Laan',
+    "phone": '345234523',
+    "email": 'anne@tandartspraktijkbvt.nl',
+    "isSick": false
+    }
+    ];
+const assistants = [
+    {​
+      "id": 5,
+      "name": 'Hanneke van',
+      "surname": 'Veen',
+      "phone": '435324251',
+      "email": 'hanneke@tandartspraktijkbvt.nl',
+      "isSick": false
+      },
+      {
+      "id": 6,
+      "name": 'Mirthe',
+      "surname": 'Timmermans',
+      "phone": '425143532',
+      "email": 'Mirthe@tandartspraktijkbvt.nl',
+      "isSick": false
+      }
+      ];
+const patients = [
+    {​
+      "id": 7,
+      "name": 'Hanneke van',
+      "surname": 'Veen',
+      "phone": '435324251',
+      "email": 'hanneke@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1980
+      },
+      {
+      "id": 8,
+      "name": 'Mirthe',
+      "surname": 'Timmermans',
+      "phone": '425143532',
+      "email": 'Mirthe@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1989
+      },
+      { 
+      "id": 9,
+      "name": 'Anne',
+      "surname": 'van der Laan',
+      "phone": '345234523',
+      "email": 'anne@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1980
+      },
+      {
+      "id": 10,
+      "name": 'Anne',
+      "surname": 'van der Laan',
+      "phone": '345234523',
+      "email": 'anne@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1988
+      },
+      {​
+      "id": 11,
+      "name": 'Hanneke van',
+      "surname": 'Veen',
+      "phone": '435324251',
+      "email": 'hanneke@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1980
+      },
+      {
+      "id": 12,
+      "name": 'Mirthe',
+      "surname": 'Timmermans',
+      "phone": '425143532',
+      "email": 'Mirthe@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1978
+      },
+      { 
+      "id": 13,
+      "name": 'Anne',
+      "surname": 'van der Laan',
+      "phone": '345234523',
+      "email": 'anne@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1967
+      },
+      {
+      "id": 14,
+      "name": 'Anne',
+      "surname": 'van der Laan',
+      "phone": '345234523',
+      "email": 'anne@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1987
+      },
+      {​
+      "id": 15,
+      "name": 'Hanneke van',
+      "surname": 'Veen',
+      "phone": '435324251',
+      "email": 'hanneke@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1995
+      },
+      {
+      "id": 16,
+      "name": 'Mirthe',
+      "surname": 'Timmermans',
+      "phone": '425143532',
+      "email": 'Mirthe@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1978
+      },
+      { 
+      "id": 17,
+      "name": 'Anne',
+      "surname": 'van der Laan',
+      "phone": '345234523',
+      "email": 'anne@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1995
+      },
+      {
+      "id": 18,
+      "name": 'Anne',
+      "surname": 'van der Laan',
+      "phone": '345234523',
+      "email": 'anne@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1959
+      },
+      {​
+      "id": 19,
+      "name": 'Hanneke van',
+      "surname": 'Veen',
+      "phone": '435324251',
+      "email": 'hanneke@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1987
+      },
+      {
+      "id": 20,
+      "name": 'Mirthe',
+      "surname": 'Timmermans',
+      "phone": '425143532',
+      "email": 'Mirthe@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1978
+      },
+      { 
+      "id": 21,
+      "name": 'Anne',
+      "surname": 'van der Laan',
+      "phone": '345234523',
+      "email": 'anne@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1957
+      },
+      {
+      "id": 22,
+      "name": 'Anne',
+      "surname": 'van der Laan',
+      "phone": '345234523',
+      "email": 'anne@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1989
+      },
+      {​
+      "id": 23,
+      "name": 'Hanneke van',
+      "surname": 'Veen',
+      "phone": '435324251',
+      "email": 'hanneke@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1998
+      },
+      {
+      "id": 24,
+      "name": 'Mirthe',
+      "surname": 'Timmermans',
+      "phone": '425143532',
+      "email": 'Mirthe@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1999
+      },
+      { 
+      "id": 25,
+      "name": 'Anne',
+      "surname": 'van der Laan',
+      "phone": '345234523',
+      "email": 'anne@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1988
+      },
+      {
+      "id": 26,
+      "name": 'Anne',
+      "surname": 'van der Laan',
+      "phone": '345234523',
+      "email": 'anne@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1990
+      },
+      {​
+      "id": 27,
+      "name": 'Hanneke van',
+      "surname": 'Veen',
+      "phone": '435324251',
+      "email": 'hanneke@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1980
+      },
+      {
+      "id": 28,
+      "name": 'Mirthe',
+      "surname": 'Timmermans',
+      "phone": '425143532',
+      "email": 'Mirthe@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1970
+      },
+      { 
+      "id": 29,
+      "name": 'Anne',
+      "surname": 'van der Laan',
+      "phone": '345234523',
+      "email": 'anne@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1978
+      },
+      {
+      "id": 30,
+      "name": 'Anne',
+      "surname": 'van der Laan',
+      "phone": '345234523',
+      "email": 'anne@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1989
+      },
+      {​
+      "id": 31,
+      "name": 'Hanneke van',
+      "surname": 'Veen',
+      "phone": '435324251',
+      "email": 'hanneke@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1970
+      },
+      {
+      "id": 32,
+      "name": 'Mirthe',
+      "surname": 'Timmermans',
+      "phone": '425143532',
+      "email": 'Mirthe@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1978
+      },
+      { 
+      "id": 33,
+      "name": 'Anne',
+      "surname": 'van der Laan',
+      "phone": '345234523',
+      "email": 'anne@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1987
+      },
+      {
+      "id": 34,
+      "name": 'Anne',
+      "surname": 'van der Laan',
+      "phone": '345234523',
+      "email": 'anne@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1968
+      },
+      {​
+      "id": 35,
+      "name": 'Hanneke van',
+      "surname": 'Veen',
+      "phone": '435324251',
+      "email": 'hanneke@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1978
+      },
+      {
+      "id": 36,
+      "name": 'Mirthe',
+      "surname": 'Timmermans',
+      "phone": '425143532',
+      "email": 'Mirthe@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1968
+      },
+      { 
+      "id": 37,
+      "name": 'Anne',
+      "surname": 'van der Laan',
+      "phone": '345234523',
+      "email": 'anne@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1989
+      },
+      {
+      "id": 38,
+      "name": 'Anne',
+      "surname": 'van der Laan',
+      "phone": '345234523',
+      "email": 'anne@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1990
+      },
+      {​
+      "id": 39,
+      "name": 'Hanneke van',
+      "surname": 'Veen',
+      "phone": '435324251',
+      "email": 'hanneke@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1990
+      },
+      {
+      "id": 40,
+      "name": 'Mirthe',
+      "surname": 'Timmermans',
+      "phone": '425143532',
+      "email": 'Mirthe@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1989
+      },
+      { 
+      "id": 41,
+      "name": 'Anne',
+      "surname": 'van der Laan',
+      "phone": '345234523',
+      "email": 'anne@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1999
+      },
+      {
+      "id": 42,
+      "name": 'Anne',
+      "surname": 'van der Laan',
+      "phone": '345234523',
+      "email": 'anne@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1988
+      },
+      {​
+      "id": 43,
+      "name": 'Hanneke van',
+      "surname": 'Veen',
+      "phone": '435324251',
+      "email": 'hanneke@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1978
+      },
+      {
+      "id": 44,
+      "name": 'Mirthe',
+      "surname": 'Timmermans',
+      "phone": '425143532',
+      "email": 'Mirthe@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1987
+      },
+      { 
+      "id": 45,
+      "name": 'Anne',
+      "surname": 'van der Laan',
+      "phone": '345234523',
+      "email": 'anne@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1988
+      },
+      {
+      "id": 46,
+      "name": 'Anne',
+      "surname": 'van der Laan',
+      "phone": '345234523',
+      "email": 'anne@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1977
+      },
+      {​
+      "id": 47,
+      "name": 'Hanneke van',
+      "surname": 'Veen',
+      "phone": '435324251',
+      "email": 'hanneke@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1990
+      }, 
+      {
+      "id": 48,
+      "name": 'Mirthe',
+      "surname": 'Timmermans',
+      "phone": '425143532',
+      "email": 'Mirthe@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1979
+      },
+      { 
+      "id": 49,
+      "name": 'Anne',
+      "surname": 'van der Laan',
+      "phone": '345234523',
+      "email": 'anne@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1999
+      },
+      {
+      "id": 50,
+      "name": 'Anne',
+      "surname": 'van der Laan',
+      "phone": '345234523',
+      "email": 'anne@tandartspraktijkbvt.nl',
+      "isSick": false,
+      "birthYear": 1990
+      }
+  ];
+
+
+
+const getRandomName = () => {
+  const state = state[Math.floor(Math.random() * 250)];
+  return `${state.name} ${state.surname}`;
+};
+
+console.log(getRandomName)
+
+const getRandomTime = () => {
+  let goodTime = false;
+  let hour;
+  while (goodTime === false) {
+    hour = Math.floor(Math.random() * 24);
+    if (hour > 7 && hour < 19) {
+      goodTime = true;
+    }
+  }
+  return `${hour}:00u`;
+};
+
+const createDayView = () => {
+  const appointments = [];
+
+  for (i = 0; i < 30; i++) {
+    appointments.push(`<li class="appointment">
+      <div class="time">${getRandomTime()}</div>
+      <div class="patient">Patiënt: ${getRandomName()}</div>
+      <div class="dentist">Tandarts: ${getRandomName()}</div>
+      <div class="assistant">Assistent: ${getRandomName()}</div>
+      </li>`);
+  }
+  document.querySelector(".dayview").innerHTML = appointments.join("");
+};
+
+const createDayOnCalendar = numAppointments => {
+  const appointments = [];
+  for (let i = 0; i < numAppointments; i++) {
+    appointments.push(`
+      <div class="appointment">
+        <span class="time">${getRandomTime()}</span>
+        <span class="patient">${getRandomName()}</span>
+      </div>`);
+  }
+  return `<div class="day">${appointments.join("")}</div>`;
+};
+
+const createCalendarView = () => {
+  const days = [];
+
+  for (let i = 1; i < 28; i++) {
+    if ([6, 7, 13, 14, 20, 21, 27, 28].includes(i)) {
+      continue;
+    }
+    days.push(createDayOnCalendar(5));
+  }
+  document.querySelector(".calendarview .table").innerHTML = days.join("");
+};
+
+const start = () => {
+  try {
+    createDayView();
+  } catch {}
+
+  try {
+    createCalendarView();
+  } catch {}
+};
+
+//hieronder is de code die ik heb gekregen, behalve de DOMContentLoaded
+
+//voet tantarts toe
+// newState = addDentist(state, "Toos", "Trekker", "06-12345678", "toos@tandartspraktijkbvt.nl")
+
+// //voeg patient toe
+// newState = addPatient(state, "Piet", "Auw", "06-12345679", "piet@wincacademy.nl", 1985)
+
+// //tantarts ziek, elke afspraak met hem/haar krijgt een rode achtergrond
+// newState = makeDentistSick(state, dentistId)
+
+// //afspraak zonder assisstent toevoegen
+// newState = addAppointment(state, dayNumber, time, patientId, dentistId)
+
+// //afspraak met assisstent toevoegen
+// newState = addAppointment(state, dayNumber, time, patientId, dentistId, assistentId)
+
+// //verwijder een afspraak
+// newState = removeAppointment(state, appointmentId)
+
+// //patient is ziek, verwijder alle afspraken met hem/haar
+// newState = makePatientSick(state, patientId)
+
+// //verplaats afspraak
+// newState = moveAppointment(state, appointmentId, newDayNumber, newTime)
+
+document.addEventListener("DOMContentLoaded", start);
